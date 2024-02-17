4bc157ab9b30578656bb5e951d3063d3ebacda00
masum <mdmasum.shuvo.cse@gmail.com>
2024-02-17 23:17:19 +0600

    admob integration

--- app/src/main/java/com/appifly/tvchannel/ui/admob/AdmobBanner().kt
+++ app/src/main/java/com/appifly/tvchannel/ui/admob/AdmobBanner().kt
@@ -4,15 +4,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.appifly.tvchannel.BuildConfig
+import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 
 @Composable
-fun AdmobBanner() {
-
-    AndroidView(modifier = Modifier.fillMaxWidth(), factory = { context ->
+fun AdmobBanner(modifier :Modifier= Modifier) {
+    AndroidView(modifier =modifier.fillMaxWidth(), factory = { context ->
         AdView(context).apply {
             setAdSize(AdSize.BANNER)
+            adUnitId=BuildConfig.BANNER_ADD_ID
+            loadAd(AdRequest.Builder().build())
         }
 
     })
