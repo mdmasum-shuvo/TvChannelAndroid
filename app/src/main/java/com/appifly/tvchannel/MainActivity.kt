91802fbff91eab33f404cead642d52a3827284fd
masum <mdmasum.shuvo.cse@gmail.com>
2024-02-18 19:58:25 +0600

    implement interstitial ad

--- app/src/main/java/com/appifly/tvchannel/MainActivity.kt
+++ app/src/main/java/com/appifly/tvchannel/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.appifly.tvchannel
 
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,10 +19,19 @@ import com.appifly.tvchannel.routing.Routing
 import com.appifly.tvchannel.ui.theme.TvChannelTheme
 import com.appifly.tvchannel.ui.view.category.CategoryScreen
 import com.appifly.tvchannel.ui.view.channel_screen.ChannelScreen
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import dagger.hilt.android.AndroidEntryPoint
+
+private var mInterstitialAd: InterstitialAd? = null
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private final val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,20 +42,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreenView()
+                    MainScreenView(this)
                 }
             }
-
         }
+        loadInterstitialAdd(this)
     }
 
+
 }
+
 
 /**
  * @Composable fun for start destination and navigation to screen
  */
 @Composable
-private fun MainScreenView() {
+private fun MainScreenView(activity: Activity) {
     val navController = rememberNavController()
     NavHost(
         navController = navController,
@@ -55,10 +69,63 @@ private fun MainScreenView() {
         }
 
         composable(Routing.ChannelScreen.routeName) {
+            mInterstitialAd?.show(activity)
             ChannelScreen(navController = navController)
         }
     }
 }
+
+
+private fun loadInterstitialAdd(activity: Activity) {
+    val adRequest = AdRequest.Builder().build()
+
+    InterstitialAd.load(
+        activity,
+        "ca-app-pub-3940256099942544/1033173712",
+        adRequest,
+        object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                adError?.toString()?.let { Log.d(TAG, it) }
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+                //mInterstitialAd!!.show(this@MainActivity)
+            }
+        })
+    mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+        override fun onAdClicked() {
+            // Called when a click is recorded for an ad.
+            Log.d(TAG, "Ad was clicked.")
+        }
+
+        override fun onAdDismissedFullScreenContent() {
+            // Called when ad is dismissed.
+            Log.d(TAG, "Ad dismissed fullscreen content.")
+            mInterstitialAd = null
+        }
+
+        override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+            // Called when ad fails to show.
+            Log.e(TAG, "Ad failed to show fullscreen content.")
+            mInterstitialAd = null
+        }
+
+        override fun onAdImpression() {
+            // Called when an impression is recorded for an ad.
+            Log.d(TAG, "Ad recorded an impression.")
+        }
+
+        override fun onAdShowedFullScreenContent() {
+            // Called when ad is shown.
+            Log.d(TAG, "Ad showed fullscreen content.")
+        }
+    }
+
+}
+
 
 
 
