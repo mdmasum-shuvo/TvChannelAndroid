061ae72774eba27c024fca32a8243613c00612f2
masum <mdmasum.shuvo.cse@gmail.com>
2024-03-01 00:04:43 +0600

    data implement

--- app_data_source/src/main/java/com/appifly/app_data_source/viewmodel/CategoryViewModel.kt
+++ app_data_source/src/main/java/com/appifly/app_data_source/viewmodel/CategoryViewModel.kt
@@ -1,22 +1,10 @@
 package com.appifly.app_data_source.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
-import androidx.lifecycle.viewModelScope
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import com.appifly.app_data_source.data.CategoryListUseCase
 import com.appifly.app_data_source.datamapper.toDto
-import com.appifly.app_data_source.dto.CategoryDto
-import com.appifly.app_data_source.worker.DataLoadWorker
 import com.appifly.cachemanager.dao.CategoryDao
-import com.appifly.network.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
