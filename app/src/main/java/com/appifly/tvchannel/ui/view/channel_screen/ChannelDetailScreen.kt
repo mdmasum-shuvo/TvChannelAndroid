cfeb613b3eabe4c4283a019ffd89c5bdef5757c8
masum <mdmasum.shuvo.cse@gmail.com>
2024-03-18 01:07:01 +0600

    implement progured rules

--- app/src/main/java/com/appifly/tvchannel/ui/view/channel_screen/ChannelDetailScreen.kt
+++ app/src/main/java/com/appifly/tvchannel/ui/view/channel_screen/ChannelDetailScreen.kt
@@ -24,8 +24,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -39,7 +37,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -49,7 +46,6 @@ import com.appifly.app_data_source.viewmodel.ChannelViewModel
 import com.appifly.tvchannel.MainActivity
 import com.appifly.tvchannel.R
 import com.appifly.tvchannel.player.ExoPlayerScreen
-import com.appifly.tvchannel.ui.admob.AdmobBanner
 import com.appifly.tvchannel.ui.admob.AdmobBannerAdaptive
 import com.appifly.tvchannel.ui.common_component.GradientFavIcon
 import com.appifly.tvchannel.ui.common_component.MainTopBar
