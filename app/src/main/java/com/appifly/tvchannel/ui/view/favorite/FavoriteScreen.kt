c5fe461dbfe7e2e28581906ff244d68ea381fa1e
masum <mdmasum.shuvo.cse@gmail.com>
2024-03-21 00:28:24 +0600

    share and about us

--- app/src/main/java/com/appifly/tvchannel/ui/view/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/appifly/tvchannel/ui/view/favorite/FavoriteScreen.kt
@@ -75,7 +75,7 @@ fun FavoriteScreen(
             HeaderText(title = context.getString(R.string.favorites_screen_title))
             categoryViewModel.favoriteCategoryList.observeAsState().value?.let { list ->
                 LazyVerticalGrid(
-                    modifier = Modifier.height(((330 * list.size) / 2).dp),
+                    modifier = Modifier.height(((400 * list.size) / 2).dp),
                     columns = GridCells.Fixed(MaterialTheme.dimens.gridCellsFavorite),
                     horizontalArrangement = Arrangement.spacedBy(12.dp),
                     verticalArrangement = Arrangement.spacedBy(12.dp),
