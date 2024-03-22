89894637fa8219b309608dcbbe91d7a82be83115
masum <mdmasum.shuvo.cse@gmail.com>
2024-03-22 18:50:19 +0600

    see all channel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.appifly.tvchannel"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
