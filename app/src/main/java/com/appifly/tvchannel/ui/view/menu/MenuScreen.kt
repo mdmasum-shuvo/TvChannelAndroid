4d469108e4eba6ea1488c0578eeb7c46d73332ce
masum <mdmasum.shuvo.cse@gmail.com>
2024-04-05 00:21:09 +0600

    splash screen implement

--- app/src/main/java/com/appifly/tvchannel/ui/view/menu/MenuScreen.kt
+++ app/src/main/java/com/appifly/tvchannel/ui/view/menu/MenuScreen.kt
@@ -47,7 +47,7 @@ fun MenuScreen(navController: NavController) {
             MenuItem(drawableId = R.drawable.about_us_icon, context.getString(R.string.about_us),context.getString(R.string.about_us_desc) ){
                 val openURL = Intent(Intent.ACTION_VIEW)
                 openURL.data =
-                    Uri.parse("https://support.shohay.health/")
+                    Uri.parse("https://play.google.com/store/apps/developer?id=Nuveq+Soft")
                 ContextCompat.startActivity(context, openURL, null)
             }
         }
