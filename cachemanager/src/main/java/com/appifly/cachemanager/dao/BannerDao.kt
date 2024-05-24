719d50c3ba913e3c9f84838e33f4bd5695804730
masum <mdmasum.shuvo.cse@gmail.com>
2024-05-24 13:00:40 +0600

    solve Grid item size issue

--- cachemanager/src/main/java/com/appifly/cachemanager/dao/BannerDao.kt
+++ cachemanager/src/main/java/com/appifly/cachemanager/dao/BannerDao.kt
@@ -5,15 +5,27 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.appifly.cachemanager.model.BannerEntity
+import androidx.room.Transaction
 import com.appifly.cachemanager.model.BannerChannelJoin
+import com.appifly.cachemanager.model.BannerEntity
 
 @Dao
 interface BannerDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(list: List<BannerEntity>)
+    suspend fun insertAll(list: List<BannerEntity>): List<Long>
 
     @Query("select  banner_table.imageUrl,banner_table.date,banner_table.title,banner_table.channelId,channel_table.iconUrl,channel_table.catId,channel_table.name ,channel_table.liveUrl from banner_table  LEFT JOIN channel_table Where banner_table.channelId=channel_table.id")
     fun getAllBanner(): LiveData<List<BannerChannelJoin>>?
+
+    @Transaction
+    suspend fun updateData(data: List<BannerEntity>): List<Long> {
+        deleteAll()
+        return insertAll(data)
+    }
+
+
+
+    @Query("DELETE FROM BANNER_TABLE")
+    fun deleteAll()
 }
