08333c2af73930235c892a46131f59a823469f43
masum <mdmasum.shuvo.cse@gmail.com>
2024-05-25 23:31:59 +0600

    push notification

--- app/src/main/java/com/appifly/tvchannel/notification/FirebaseMessaging.kt
+++ app/src/main/java/com/appifly/tvchannel/notification/FirebaseMessaging.kt
@@ -1,8 +1,20 @@
 package com.appifly.tvchannel.notification
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_DEFAULT
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.appifly.tvchannel.MainActivity
+import com.appifly.tvchannel.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class FirebaseMessaging :FirebaseMessagingService() {
@@ -24,6 +36,31 @@ class FirebaseMessaging :FirebaseMessagingService() {
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                addFlags(FLAG_ACTIVITY_CLEAR_TOP)
+            }
+
+            val pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, FLAG_IMMUTABLE
+            )
+
+            val channelId = this.getString(R.string.default_notification_channel_id)
+
+            val notificationBuilder = NotificationCompat.Builder(this, channelId)
+                .setContentTitle(remoteMessage.notification?.title)
+                .setContentText(remoteMessage.notification?.body)
+                .setSmallIcon(android.R.drawable.stat_notify_sync)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(channelId, "channel", IMPORTANCE_DEFAULT)
+                manager.createNotificationChannel(channel)
+            }
+
+            manager.notify(Random.nextInt(), notificationBuilder.build())
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
