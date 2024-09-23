87b7441dee4048b72d5b3d85debc392d9e9ce885
masum <mdmasum.shuvo.cse@gmail.com>
2024-09-23 21:29:30 +0600

    add new exoplayer with togle change

--- app/src/main/java/com/appifly/tvchannel/player/PlayVideo.kt
+++ app/src/main/java/com/appifly/tvchannel/player/PlayVideo.kt
@@ -1,5 +1,6 @@
 package com.appifly.tvchannel.player
 
+import android.net.Uri
 import androidx.annotation.OptIn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -11,12 +12,11 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.media3.common.MediaItem
-import androidx.media3.common.MediaMetadata
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.datasource.HttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.hls.HlsMediaSource
-import androidx.media3.exoplayer.source.MediaSource
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import com.appifly.app_data_source.dto.ChannelDto
 import com.appifly.app_data_source.viewmodel.ChannelViewModel
 import com.appifly.tvchannel.utils.Constants.PLAYER_SEEK_BACK_INCREMENT
@@ -31,9 +31,6 @@ fun PlayVideo(
     channelViewModel: ChannelViewModel
 ) {
     val context = LocalContext.current
-    /*    val mediaItems = MediaItem.Builder().setUri("https://sfux-ext.sfux.info/hls/chapter/105/1588724110/1588724110.m3u8").setMediaId("1").setTag("tag")
-            .setMediaMetadata(MediaMetadata.Builder().setDisplayTitle("test").build())
-            .build()*/
     val lifecycle = LocalLifecycleOwner.current.lifecycle
 
     val exoPlayer = remember {
@@ -84,35 +81,16 @@ fun PlayVideo(
     )
 }
 
-
-@OptIn(UnstableApi::class)
-private fun getHlsMediaSource(
-    dataSourceFactory: DataSource.Factory,
-    mediaUrl: String
-): MediaSource {
-    // Create a HLS media source pointing to a playlist uri.
-    return HlsMediaSource.Factory(dataSourceFactory).createMediaSource(MediaItem.fromUri(mediaUrl))
-}
-
 @OptIn(UnstableApi::class)
 fun playerReadyToPlay(data: ChannelDto, exoPlayer: ExoPlayer) {
-    /*    val defaultDataSourceFactory = DefaultDataSource.Factory(context)
-        val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(
-            context,
-            defaultDataSourceFactory
-        )
-        val source =
-            getHlsMediaSource(dataSourceFactory, videoUrl)
-        *//*  else
-      getProgressiveMediaSource(dataSourceFactory, videoUrl.value!!.liveUrl!!)*//*
-
-    exoPlayer.setMediaSource(source)
+    val httpDataSourceFactory: HttpDataSource.Factory = DefaultHttpDataSource.Factory()
+    data.liveChannelReferer?.let {
+        httpDataSourceFactory.setDefaultRequestProperties(mapOf("Referer" to it))
+    }
+    val mediaSourceFactory = DefaultMediaSourceFactory(httpDataSourceFactory)
+    // Create a media item
+    val mediaItem = MediaItem.fromUri(Uri.parse(data.liveUrl))
+    exoPlayer.setMediaSource(mediaSourceFactory.createMediaSource(mediaItem))
     exoPlayer.prepare()
-    exoPlayer.playWhenReady = true*/
-    val mediaItems = MediaItem.Builder().setUri(data.liveUrl).setMediaId(data.id.toString()).setTag("tag")
-        .setMediaMetadata(MediaMetadata.Builder().setDisplayTitle(data.name).build())
-        .build()
-    exoPlayer.setMediaItem(mediaItems)
-    exoPlayer.prepare()
-    exoPlayer.playWhenReady = true
+    exoPlayer.play()
 }
