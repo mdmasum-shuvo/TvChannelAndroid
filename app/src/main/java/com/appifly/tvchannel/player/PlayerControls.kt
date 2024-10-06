6759901255903407b6c6b93d3a478b6628fc7fb6
masum <mdmasum.shuvo.cse@gmail.com>
2024-10-07 00:03:56 +0600

    player state change

--- app/src/main/java/com/appifly/tvchannel/player/PlayerControls.kt
+++ app/src/main/java/com/appifly/tvchannel/player/PlayerControls.kt
@@ -70,199 +70,202 @@ fun PlayerControls(
     val context = LocalContext.current
 
 
-    if (playing.not() && playerState == STATE_BUFFERING) {
-        Loader()
-    }
-    AnimatedVisibility(
-        modifier = modifier,
-        visible = visible,
-        enter = fadeIn(),
-        exit = fadeOut()
-    ) {
-        Box(
-            modifier = Modifier
-                .testTag("PlayerControlsParent")
-                .background(MaterialTheme.colorScheme.background.copy(alpha = 0.6f))
+
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        AnimatedVisibility(
+            modifier = modifier,
+            visible = visible,
+            enter = fadeIn(),
+            exit = fadeOut()
         ) {
-
-            Text(
+            Box(
                 modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .padding(16.dp)
-                    .testTag("VideoTitle")
-                    .animateEnterExit(
-                        enter = slideInVertically(
-                            initialOffsetY = { fullHeight: Int -> -fullHeight }
-                        ),
-                        exit = shrinkVertically()
-                    ),
-                text = title,
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onBackground
-            )
-
-            val controlButtonModifier: Modifier = remember(isFullScreen) {
-
-                if (isFullScreen) {
-                    Modifier
-                        .padding(horizontal = 8.dp)
-                        .size(40.dp)
-                } else {
-                    Modifier.size(32.dp)
-                }
-            }
-
-            Row(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .fillMaxWidth()
-                    .testTag("VideoControlParent"),
-                horizontalArrangement = if (isFullScreen) {
-                    Arrangement.Center
-                } else {
-                    Arrangement.SpaceEvenly
-                }
+                    .testTag("PlayerControlsParent")
+                    .background(MaterialTheme.colorScheme.background.copy(alpha = 0.6f))
             ) {
-                IconButton(
-                    modifier = controlButtonModifier,
-                    onClick = onPrevious
-                ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(id = androidx.media3.ui.R.drawable.exo_ic_skip_previous),
-                        contentDescription = stringResource(id = R.string.play_previous)
-                    )
-                }
 
-                IconButton(
-                    modifier = controlButtonModifier,
-                    onClick = onReplay
-                ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(id = R.drawable.ic_replay_5),
-                        contentDescription = stringResource(id = R.string.rewind_5)
-                    )
-                }
-
-                IconButton(
-                    modifier = controlButtonModifier,
-                    onClick = onPauseToggle
-                ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(
-                            id =
-                            when {
-                                playing -> {
-                                    R.drawable.pause_button
-                                }
-
-                                playing.not() && playerState == STATE_ENDED -> {
-                                    R.drawable.play_button
-                                }
-
-                                else -> {
-                                    R.drawable.play_button
-                                }
-                            }
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .padding(16.dp)
+                        .testTag("VideoTitle")
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                initialOffsetY = { fullHeight: Int -> -fullHeight }
+                            ),
+                            exit = shrinkVertically()
                         ),
-                        contentDescription = stringResource(id = R.string.toggle_play)
-                    )
-                }
+                    text = title,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
 
-                IconButton(
-                    modifier = controlButtonModifier,
-                    onClick = onForward
-                ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(id = R.drawable.ic_forward_10),
-                        contentDescription = stringResource(id = R.string.forward_10)
-                    )
-                }
+                val controlButtonModifier: Modifier = remember(isFullScreen) {
 
-                IconButton(
-                    modifier = controlButtonModifier,
-                    onClick = onNext
-                ) {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(id = androidx.media3.ui.R.drawable.exo_ic_skip_next),//R.drawable.ic_skip_next
-                        contentDescription = stringResource(id = R.string.play_next)
-                    )
+                    if (isFullScreen) {
+                        Modifier
+                            .padding(horizontal = 8.dp)
+                            .size(40.dp)
+                    } else {
+                        Modifier.size(32.dp)
+                    }
                 }
-            }
-
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .fillMaxWidth()
-                    .padding(bottom = if (isFullScreen) 32.dp else 16.dp)
-                    .testTag("VideoSeek")
-                    .animateEnterExit(
-                        enter = slideInVertically(
-                            initialOffsetY = { fullHeight: Int -> fullHeight }
-                        ),
-                        exit = slideOutVertically(
-                            targetOffsetY = { fullHeight: Int -> fullHeight }
-                        )
-                    )
-            ) {
 
                 Row(
                     modifier = Modifier
+                        .align(Alignment.Center)
                         .fillMaxWidth()
-                        .padding(top = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                        .testTag("VideoControlParent"),
+                    horizontalArrangement = if (isFullScreen) {
+                        Arrangement.Center
+                    } else {
+                        Arrangement.SpaceEvenly
+                    }
                 ) {
-                    Box {
-
+                    IconButton(
+                        modifier = controlButtonModifier,
+                        onClick = onPrevious
+                    ) {
+                        Image(
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(id = androidx.media3.ui.R.drawable.exo_ic_skip_previous),
+                            contentDescription = stringResource(id = R.string.play_previous)
+                        )
                     }
 
                     IconButton(
-                        modifier = Modifier
-                            .testTag("FullScreenToggleButton")
-                            .padding(end = 16.dp)
-                            .size(24.dp)
-                            .animateEnterExit(
-                                enter = slideInVertically(
-                                    initialOffsetY = { fullHeight: Int -> fullHeight }
-                                ),
-                                exit = slideOutVertically(
-                                    targetOffsetY = { fullHeight: Int -> fullHeight }
-                                )
-                            ),
-                        onClick = {
-                            if (isFullScreen.not()) {
-                                context.setLandscape()
-                            } else {
-                                context.setPortrait()
-                            }.also {
-                                onFullScreenToggle.invoke(isFullScreen.not())
-                            }
-                        }
+                        modifier = controlButtonModifier,
+                        onClick = onReplay
+                    ) {
+                        Image(
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(id = R.drawable.ic_replay_5),
+                            contentDescription = stringResource(id = R.string.rewind_5)
+                        )
+                    }
+
+                    IconButton(
+                        modifier = controlButtonModifier,
+                        onClick = onPauseToggle
                     ) {
                         Image(
                             modifier = Modifier.fillMaxSize(),
                             contentScale = ContentScale.Crop,
                             painter = painterResource(
-                                id = if (isFullScreen) {
-                                    R.drawable.full_screen_exit
-                                } else {
-                                    R.drawable.full_screen_entry
+                                id =
+                                when {
+                                    playing -> {
+                                        R.drawable.pause_button
+                                    }
+
+                                    playing.not() && playerState == STATE_ENDED -> {
+                                        R.drawable.play_button
+                                    }
+
+                                    else -> {
+                                        R.drawable.play_button
+                                    }
                                 }
                             ),
-                            contentDescription = stringResource(id = R.string.toggle_full_screen)
+                            contentDescription = stringResource(id = R.string.toggle_play)
+                        )
+                    }
+
+                    IconButton(
+                        modifier = controlButtonModifier,
+                        onClick = onForward
+                    ) {
+                        Image(
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(id = R.drawable.ic_forward_10),
+                            contentDescription = stringResource(id = R.string.forward_10)
+                        )
+                    }
+
+                    IconButton(
+                        modifier = controlButtonModifier,
+                        onClick = onNext
+                    ) {
+                        Image(
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(id = androidx.media3.ui.R.drawable.exo_ic_skip_next),//R.drawable.ic_skip_next
+                            contentDescription = stringResource(id = R.string.play_next)
                         )
                     }
                 }
+
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth()
+                        .padding(bottom = if (isFullScreen) 32.dp else 16.dp)
+                        .testTag("VideoSeek")
+                        .animateEnterExit(
+                            enter = slideInVertically(
+                                initialOffsetY = { fullHeight: Int -> fullHeight }
+                            ),
+                            exit = slideOutVertically(
+                                targetOffsetY = { fullHeight: Int -> fullHeight }
+                            )
+                        )
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Box {
+
+                        }
+
+                        IconButton(
+                            modifier = Modifier
+                                .testTag("FullScreenToggleButton")
+                                .padding(end = 16.dp)
+                                .size(24.dp)
+                                .animateEnterExit(
+                                    enter = slideInVertically(
+                                        initialOffsetY = { fullHeight: Int -> fullHeight }
+                                    ),
+                                    exit = slideOutVertically(
+                                        targetOffsetY = { fullHeight: Int -> fullHeight }
+                                    )
+                                ),
+                            onClick = {
+                                if (isFullScreen.not()) {
+                                    context.setLandscape()
+                                } else {
+                                    context.setPortrait()
+                                }.also {
+                                    onFullScreenToggle.invoke(isFullScreen.not())
+                                }
+                            }
+                        ) {
+                            Image(
+                                modifier = Modifier.fillMaxSize(),
+                                contentScale = ContentScale.Crop,
+                                painter = painterResource(
+                                    id = if (isFullScreen) {
+                                        R.drawable.full_screen_exit
+                                    } else {
+                                        R.drawable.full_screen_entry
+                                    }
+                                ),
+                                contentDescription = stringResource(id = R.string.toggle_full_screen)
+                            )
+                        }
+                    }
+                }
             }
+        }
+        if (playing.not() && playerState == STATE_BUFFERING) {
+            Loader()
         }
     }
 
